13633525615c424de296dbe21c660c382cab95e0
Henrik Andersen Sveinsson <henrik.sveinsson@me.com>
2020-04-29 09:19:24 +0200

    Changed orientation in example to show more of the cylinder geometry features

--- examples/carve_quartz_cylinder.py
+++ examples/carve_quartz_cylinder.py
@@ -3,7 +3,7 @@ from molecular_builder.geometry import CylinderGeometry
 
 atoms = create_bulk_crystal("alpha_quartz", [120, 120, 120])
 
-geometry = CylinderGeometry([60,60,60], 20, 60, orientation=[0,0,1])
+geometry = CylinderGeometry([60,60,80], 20, 90, orientation=[0.5,1,1])
 
 num_carved, carved = carve_geometry(atoms, geometry, side="in", return_carved=True)
 
