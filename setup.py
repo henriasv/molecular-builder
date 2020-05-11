0fee63d0649ae0f3b600629ec5c9472a2538757e
Henrik Andersen Sveinsson <henrik.sveinsson@me.com>
2020-05-11 15:28:22 +0200

    dependency

--- setup.py
+++ setup.py
@@ -17,5 +17,5 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.6',
 )
