66e1119f05cdd91dff9bbb997fffb9fee17a330b
Henrik Andersen Sveinsson <henrik.sveinsson@me.com>
2020-05-27 21:45:02 +0200

    updated example

--- examples/pack_water_in_voids.py
+++ examples/pack_water_in_voids.py
@@ -5,10 +5,10 @@ from molecular_builder.geometry import SphereGeometry, CubeGeometry
 # Amorphous silica of size (357, 143, 143)
 amorph = fetch_prepared_system("amorphous_silica_1")
 
-num_spheres = 20
+num_spheres = 5
 for sphere in range(num_spheres):
     i, j, k, l = np.random.uniform(size=4)
-    x, y, z, r = i*357, j*143, k*143, l*30
+    x, y, z, r = i*357, j*143, k*143, l*30+5
     geometry = SphereGeometry([x, y, z], r, periodic_boundary_condition=(True, True, True))
     tmp_carved = carve_geometry(amorph, geometry, side="in")
     print(f"tmp carved: {tmp_carved}")
