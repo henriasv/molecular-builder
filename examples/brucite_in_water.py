3eca6fc53c27fc6c048713f3367514dfcc56421b
Henrik Andersen Sveinsson <henrik.sveinsson@me.com>
2020-05-27 21:44:19 +0200

    updated example

--- examples/brucite_in_water.py
+++ examples/brucite_in_water.py
@@ -4,9 +4,9 @@ from ase.constraints import FixBondLengths
 
 atoms = create_bulk_crystal("brucite", [120, 120, 120])
 
-geometry = BlockGeometry(center=[60,60,60], 
-                         length=[40,150,100], 
-                         orientation=[[0,0,1],[1,0,0],[0,1,0]],
+geometry = BlockGeometry(center=[60,60,59.5], 
+                         length=[41,150,95], 
+                         orientation=[[1,0,0],[0,1,0],[0,0,1]],
                          periodic_boundary_condition=(True, True, True))
 
 num_carved = carve_geometry(atoms, geometry, side="out")
@@ -15,3 +15,4 @@ water = pack_water(20000, atoms=atoms, pbc=2.0)
 atoms += water
 
 write(atoms, "brucite_block.data", bond_specs=("O", "H", 1.02))
+write(atoms, "brucite_block.png", bond_specs=("O", "H", 1.02))
