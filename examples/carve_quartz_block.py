c91cf0f3de9a31e850b19bb4dfeed9aa142bd491
evenmn <evenmn@student.matnat.uio.no>
2020-09-09 14:52:12 +0200

    carve_quartz_block.data now utilizes CubeGeometry instead of BlockGeometry

--- examples/carve_quartz_block.py
+++ examples/carve_quartz_block.py
@@ -1,12 +1,9 @@
 from molecular_builder import create_bulk_crystal, carve_geometry
-from molecular_builder.geometry import BlockGeometry
+from molecular_builder.geometry import BoxGeometry
 
-atoms = create_bulk_crystal("alpha_quartz", [120, 120, 120])
+atoms = create_bulk_crystal("alpha_quartz", (120, 120, 120))
 
-geometry = BlockGeometry(center=[60,60,60], 
-                         length=[40,50,60], 
-                         orientation=[[0,0,1],[1,0,0],[0,1,0]],
-                         periodic_boundary_condition=(True, True, True))
+geometry = BoxGeometry(center=(40, 40, 40), length=(60, 60, 60))
 
 num_carved, carved = carve_geometry(atoms, geometry, side="in", return_carved=True)
 
