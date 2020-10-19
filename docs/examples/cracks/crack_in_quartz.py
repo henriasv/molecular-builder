f88d230b31a41c365f8f41316c46266debb689a0
Marthe Guren <m.g.guren@geo.uio.no>
2020-10-19 14:41:54 +0200

    fixed typo in example-file

--- docs/examples/cracks/crack_in_quartz.py
+++ docs/examples/cracks/crack_in_quartz.py
@@ -8,7 +8,7 @@ quartz_structure = create_bulk_crystal("alpha_quartz", L)
 cell = quartz_structure.get_cell(); cell[0,1] = 0; cell[1,0] = 0; cell[0,2] = 0; cell[2,0] = 0; cell[1,2] = 0; cell[2,1] = 0
 quartz_structure.set_cell(cell); quartz_structure.wrap()
 
-crack = NotchGeometry([0,L[1]/2,L[1]/2], [20,0,0], [0,0,10])
+crack = NotchGeometry([0,L[1]/2,L[2]/2], [20,0,0], [0,0,10])
 
 carve_geometry(quartz_structure, crack, side="in")
 write(quartz_structure, "alpha_quartz_with_crack.png",  camera_dir=[0, 1, 0], viewport_type="perspective")
