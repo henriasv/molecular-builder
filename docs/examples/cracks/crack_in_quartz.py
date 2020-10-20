24f10389aaf3d99049a6ba91676e41ae35993025
Henrik Andersen Sveinsson <henrik.sveinsson@me.com>
2020-10-20 20:52:38 +0200

    Update crack_in_quartz.py

--- docs/examples/cracks/crack_in_quartz.py
+++ docs/examples/cracks/crack_in_quartz.py
@@ -2,7 +2,9 @@ from molecular_builder.core import create_bulk_crystal, write, carve_geometry
 from molecular_builder.geometry import NotchGeometry
 import ase.cell 
 
-quartz_structure = create_bulk_crystal("alpha_quartz", [100, 100, 100]) 
+L = [100,50,100]
+
+quartz_structure = create_bulk_crystal("alpha_quartz", L) 
 cell = quartz_structure.get_cell(); cell[0,1] = 0; cell[1,0] = 0; cell[0,2] = 0; cell[2,0] = 0; cell[1,2] = 0; cell[2,1] = 0
 quartz_structure.set_cell(cell); quartz_structure.wrap()
 
