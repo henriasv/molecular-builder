117803407d989aae927cc30a963d882787eeb082
Henrik Andersen Sveinsson <henriasv@fys.uio.no>
2025-03-28 15:11:08 +0100

    typo

--- examples/build_quartz_block.py
+++ examples/build_quartz_block.py
@@ -2,5 +2,5 @@ from molecular_builder import create_bulk_crystal, write
 
 atoms = create_bulk_crystal("alpha_quartz", [50,100,50])
 
-write(atoms, "alpha_quartz_mb.data", bond_specs=[("Si", "O", 2.8)])
+write(atoms, "alpha_quartz_mb.data", bond_specs=[("Si", "O", 2.8)], specorder=["O", "Si"])
 write(atoms, "alpha_quartz_mb.png", bond_specs=[("Si", "O", 2.8)])
